65557f7c55cdc12dfd61d9a243ef45a63789dd09
bruce-mig <brucetmig@icloud.com>
2024-03-12 10:05:30 +0200

    login handler func implementation

--- controllers/auth_controller.go
+++ controllers/auth_controller.go
@@ -1,8 +1,12 @@
 package controllers
 
 import (
+	"strconv"
+	"time"
+
 	"github.com/bruce-mig/go-admin/db"
 	"github.com/bruce-mig/go-admin/models"
+	"github.com/dgrijalva/jwt-go/v4"
 	"github.com/gofiber/fiber/v2"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -14,6 +18,11 @@ type registerRequest struct {
 	Email          string `gorm:"unique" json:"email" `
 	Password       string `json:"password"`
 	VerifyPassword string `json:"verify_password"`
+}
+
+type loginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
 func Register(c *fiber.Ctx) error {
@@ -33,7 +42,7 @@ func Register(c *fiber.Ctx) error {
 	password, _ := bcrypt.GenerateFromPassword([]byte(data.Password), 14)
 
 	user := models.User{
-		Id:        data.Id,
+		// Id:        data.Id,
 		FirstName: data.FirstName,
 		LastName:  data.LastName,
 		Email:     data.Email,
@@ -43,4 +52,56 @@ func Register(c *fiber.Ctx) error {
 	db.DB.Create(&user)
 
 	return c.JSON(user)
+}
+
+func Login(c *fiber.Ctx) error {
+	var data loginRequest
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	var user models.User
+
+	db.DB.Where("email = ?", data.Email).First(&user)
+
+	if user.Id == 0 {
+		c.Status(404)
+		return c.JSON(fiber.Map{
+			"message": "not found",
+		})
+	}
+
+	err := bcrypt.CompareHashAndPassword(user.Password, []byte(data.Password))
+	if err != nil {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "incorrect creedentials",
+		})
+	}
+
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Issuer: strconv.Itoa(int(user.Id)),
+		ExpiresAt: &jwt.Time{
+			time.Now().Add(time.Hour * 24),
+		},
+	})
+
+	token, err := claims.SignedString([]byte("secret"))
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    token,
+		Expires:  time.Now().Add(time.Hour * 24),
+		HTTPOnly: true,
+	}
+
+	c.Cookie(&cookie)
+
+	return c.JSON(fiber.Map{
+		"message": "successfully logged in",
+	})
 }
