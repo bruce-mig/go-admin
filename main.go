1a69628e83ed77507085683d88a23a4a140f7c5b
bruce-mig <brucetmig@icloud.com>
2024-03-12 10:05:56 +0200

    added cookies and cors

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/bruce-mig/go-admin/db"
 	"github.com/bruce-mig/go-admin/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
@@ -11,8 +12,13 @@ func main() {
 
 	app := fiber.New()
 
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+		AllowOrigins:     "http://localhost:8000",
+	}))
+
 	routes.SetupRoutes(app)
 
-	app.Listen(":8000")
 	println("Listening on port 8080!")
+	app.Listen(":8000")
 }
