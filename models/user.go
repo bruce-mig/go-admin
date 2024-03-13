ec7d5cf4eba6015f5b63818b48b5e91cb70b2184
bruce-mig <brucetmig@icloud.com>
2024-03-13 14:44:32 +0200

    methods for user structs

--- models/user.go
+++ models/user.go
@@ -1,9 +1,22 @@
 package models
 
+import "golang.org/x/crypto/bcrypt"
+
 type User struct {
 	Id        uint   `gorm:"primary key;autoIncrement" json:"id"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Email     string `gorm:"unique" json:"email" `
-	Password  []byte `json:"password"`
+	Password  []byte `json:"-"`
+	RoleId    uint   `json:"role_id"`
+	Role      Role   `json:"role" gorm:"foreignKey:RoleId"`
+}
+
+func (user *User) SetPassword(password string) {
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(password), 14)
+	user.Password = hashedPassword
+}
+
+func (user *User) ComparePassword(password string) error {
+	return bcrypt.CompareHashAndPassword(user.Password, []byte(password))
 }
