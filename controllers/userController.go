29456e41fc92c7ec956f2395f7dbebe40744795a
bruce-mig <brucetmig@icloud.com>
2024-03-13 14:43:03 +0200

    user API handlers

---
+++ controllers/userController.go
@@ -0,0 +1,83 @@
+package controllers
+
+import (
+	"strconv"
+
+	"github.com/bruce-mig/go-admin/db"
+	"github.com/bruce-mig/go-admin/models"
+	"github.com/gofiber/fiber/v2"
+)
+
+func ListUsers(c *fiber.Ctx) error {
+	page, _ := strconv.Atoi(c.Query("page", "1"))
+
+	limit := 5
+	offset := (page-1)*limit
+	var total in64
+
+	var users []models.User
+
+	db.DB.Preload("Role").Offset(offset).Limit(limit).Find(&users)
+	dd.DB.Model(&models.User{}).Count(&total)
+	return c.JSON(fiber.Map{
+		"data": users,
+		"meta": fiber.Map{
+			"total": total,
+			"page": page,
+			"last_page": math.Ceil(float64(int(total)/limit)),
+		}
+	})
+}
+
+func CreateUser(c *fiber.Ctx) error {
+	var user models.User
+
+	if err := c.BodyParser(&user); err != nil {
+		return err
+	}
+
+	user.SetPassword("1234")
+	db.DB.Create(&user)
+
+	return c.JSON(user)
+}
+
+func GetUser(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	user := models.User{
+		Id: uint(id),
+	}
+
+	db.DB.Preload("Role").Find(&user)
+
+	return c.JSON(user)
+}
+
+func UpdateUser(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	user := models.User{
+		Id: uint(id),
+	}
+
+	if err := c.BodyParser(&user); err != nil {
+		return err
+	}
+
+	db.DB.Model(&user).Updates(user)
+
+	return c.JSON(user)
+}
+
+func DeleteUser(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	user := models.User{
+		Id: uint(id),
+	}
+
+	db.DB.Delete(&user)
+
+	return nil
+}
