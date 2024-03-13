ce5d9fb269f0c074b80006b0fa36d31d95a9515e
bruce-mig <brucetmig@icloud.com>
2024-03-13 14:45:51 +0200

    added user, role and permissions APIs

--- routes/routes.go
+++ routes/routes.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"github.com/bruce-mig/go-admin/middleware"
+
 	"github.com/bruce-mig/go-admin/controllers"
 	"github.com/gofiber/fiber/v2"
 )
@@ -8,4 +10,24 @@ import (
 func SetupRoutes(app *fiber.App) {
 	app.Post("/api/register", controllers.Register)
 	app.Post("/api/login", controllers.Login)
+
+	app.Use(middleware.IsAuthenticated)
+
+	app.Get("/api/user", controllers.User)
+	app.Post("/api/logout", controllers.Logout)
+
+	app.Get("/api/users", controllers.ListUsers)
+	app.Post("/api/users", controllers.CreateUser)
+	app.Get("/api/users/:id", controllers.GetUser)
+	app.Put("/api/users/:id", controllers.UpdateUser)
+	app.Delete("/api/users/:id", controllers.DeleteUser)
+
+	app.Get("/api/roles", controllers.ListRoles)
+	app.Post("/api/roles", controllers.CreateRole)
+	app.Get("/api/roles/:id", controllers.GetRole)
+	app.Put("/api/roles/:id", controllers.UpdateRole)
+	app.Delete("/api/roles/:id", controllers.DeleteRole)
+
+	app.Get("/api/permissions", controllers.ListPermissions)
+	app.Post("/api/permissions", controllers.CreatePermission)
 }
