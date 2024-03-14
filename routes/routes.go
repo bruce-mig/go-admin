8e492f7457302564a0dcbb97591b19f3fa81fbe4
bruce-mig <brucetmig@icloud.com>
2024-03-14 13:54:59 +0200

    added file server and upload API

--- routes/routes.go
+++ routes/routes.go
@@ -39,4 +39,7 @@ func SetupRoutes(app *fiber.App) {
 	app.Get("/api/products/:id", controllers.GetProduct)
 	app.Put("/api/products/:id", controllers.UpdateProduct)
 	app.Delete("/api/products/:id", controllers.DeleteProduct)
+
+	app.Post("/api/upload", controllers.Upload)
+	app.Static("/api/uploads", "./uploads")
 }
